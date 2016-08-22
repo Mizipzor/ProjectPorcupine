4f66701d53c397de499858a8a3b492d5b1d2c21c
Alexander Fast <mizipzor@gmail.com>
2016-08-22 20:46:20 +0200

    Changed to using npm unitysolution.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,16 +5,7 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="ProjectPorcupine"
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+unitysolution ./ Standalone
 
+xbuild /p:Configuration=Debug ProjectPorcupine.sln
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
