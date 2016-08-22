2976552e27d2df0a6a11e71c850939bd1a768b6c
Alexander Fast <mizipzor@gmail.com>
2016-08-22 21:01:17 +0200

    Another build script fix.

--- Build.sh
+++ Build.sh
@@ -1,11 +1,5 @@
-#! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ProjectPorcupine"
-
-unitysolution ./ Standalone
+unitysolution ./ProjectPorcupine Standalone
 
 xbuild /p:Configuration=Debug ProjectPorcupine.sln
 
